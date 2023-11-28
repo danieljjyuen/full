8d47805b994ef0a60f4ffdf147556912ffb3ca79
danieljjyuen <dan@LAPTOP-AP8VLLC3.localdomain>
2023-11-28 14:50:31 -0500

    part 10 exercise 10.4 - 10.5

---
+++ rate-repository-app/src/components/AppBar.jsx
@@ -0,0 +1,25 @@
+import { View, StyleSheet, Text,Pressable} from 'react-native';
+import Constants from 'expo-constants';
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: theme.colors.primary,
+    paddingBottom: Constants.statusBarHeight,
+    paddingLeft: 15
+  },
+  // ...
+});
+
+const AppBar = () => {
+  return( 
+  <View style={styles.container}>
+    <Pressable>
+        <Text>Repositories</Text>
+    </Pressable>
+  </View>
+  )
+};
+
+export default AppBar;
--- rate-repository-app/src/components/RepositoryItem.jsx
+++ rate-repository-app/src/components/RepositoryItem.jsx
@@ -1,16 +1,124 @@
-import { Text, View } from 'react-native';
+    import { Text, View,Image, StyleSheet } from 'react-native';
+    import theme from '../theme';
+    const style = StyleSheet.create({
+        containerRow:{
+            flexDirection: 'row',
+        },
+        containerColumn:{
+            flexDirection:'column',
+            backgroundColor:'white'
+        },
+        column:{
+            flexDirection:'column',
+            marginLeft:10
+        },
+        containerColumnStat:{
+            flexDirection:'column',
+            alignItems:'center',
+            flex:1
+        },
+        containerCenterRow:{
+            alignItems: 'center',
+            flex:1,
+            flexDirection:'row'
+        },
+        avatar:{
+            width:50,
+            height:50,
+            borderRadius: 25
+        },
+        fullName:{
+            fontWeight:'bold',
+            fontSize:18
+        },
+        statBold:{
+            fontWeight:'bold',
+        },
+        textBackground: {
+            backgroundColor: theme.colors.primary,
+            padding: 5,
+            borderRadius: 5,
+            alignSelf: 'flex-start'
+            
+        },
+        flexContainer:{
+            marginTop: 5,
+            overflow: 'hidden',
+            borderRadius: 5,
+        }
+    })
 
-const RepositoryItem = ({item}) => {
-    return(
-        <View>
-            <Text>Full name:{item.fullName}</Text>
-            <Text>Description:{item.description}</Text>
-            <Text>Language:{item.language}</Text>
-            <Text>Stars:{item.stargazersCount}</Text>
-            <Text>Forks:{item.forksCount}</Text>
-            <Text>Reviews:{item.reviewCount}</Text>
-            <Text>Rating:{item.ratingAverage}</Text>
-        </View>
-    )
-}
-export default RepositoryItem
+    function formatNumber(number) {
+        if (number >= 1000) {
+          const roundedNumber = Math.round(number / 100) / 10;
+          return `${roundedNumber}k`;
+        } else {
+          return number.toString();
+        }
+      }
+
+    const RepositoryItem = ({item}) => {
+        return(
+        <View style={style.containerColumn}>
+            <View style={style.containerRow}> 
+                <Image style={style.avatar} source={{uri:item.ownerAvatarUrl}}/>
+                
+                <View style={style.column}>
+                <Text style={style.fullName}>{item.fullName}</Text>
+                <Text>{item.description}</Text>
+    
+                <View style={style.flexContainer}>
+                <View style={style.textBackground}>
+                <Text>{item.language}</Text>
+                </View>
+            </View>
+                
+                </View>
+
+
+
+
+                
+                </View>
+
+
+                <View style={style.containerCenterRow}>
+                    <View style={style.containerColumnStat}>
+                    <Text style={style.statBold}>{formatNumber(item.stargazersCount)}</Text>
+                    <Text>Stars</Text>
+                    </View>
+
+                    <View style={style.containerColumnStat}>
+                    <Text style={style.statBold}>{formatNumber(item.forksCount)}</Text>
+                    <Text>Forks</Text>
+                    </View>
+                    <View style={style.containerColumnStat}>
+                    <Text style={style.statBold}>{formatNumber(item.reviewCount)}</Text>
+                    <Text>Reviews</Text>
+                    </View>
+                
+                    <View style={style.containerColumnStat}>
+                    <Text style={style.statBold}>{formatNumber(item.ratingAverage)}</Text>
+                    <Text>Rating</Text>
+                    </View>
+                
+                
+                </View>
+
+
+            </View>
+        )
+    }
+    export default RepositoryItem
+
+    // {
+    //     id: 'jaredpalmer.formik',
+    //     fullName: 'jaredpalmer/formik',
+    //     description: 'Build forms in React, without the tears',
+    //     language: 'TypeScript',
+    //     forksCount: 1589,
+    //     stargazersCount: 21553,
+    //     ratingAverage: 88,
+    //     reviewCount: 4,
+    //     ownerAvatarUrl: 'https://avatars2.githubusercontent.com/u/4060187?v=4',
+    //   },
