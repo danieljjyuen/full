73c589aeec38d9d55c6858e7e77a129a1d0f3682
danieljjyuen <dan@LAPTOP-AP8VLLC3.localdomain>
2023-12-08 22:50:44 -0500

    part 10 exercise 10.12 environment variables

--- rate-repository-app/src/utils/apolloClient.js
+++ rate-repository-app/src/utils/apolloClient.js
@@ -1,8 +1,9 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import Constants from 'expo-constants'
 
 const httpLink = createHttpLink({
   // Replace the IP address part with your own IP address!
-  uri: 'http://172.22.8.190:4000/graphql',
+  uri: Constants.manifest.extra.apollo_uri 
 });
 
 const createApolloClient = () => {
